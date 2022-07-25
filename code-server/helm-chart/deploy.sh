79fb849db2d12b770dee86683b1a0a45faaa03f0
kimhaksoo <hskim@hancom.com>
2022-07-25 13:03:23 +0900

    rebuilding site 2022년 07월 25일 월 오후  1:03:23

--- code-server/helm-chart/deploy.sh
+++ code-server/helm-chart/deploy.sh
@@ -8,7 +8,7 @@ git submodule update
 # Build the package.
 helm package .  
 helm repo index .
-mv code-server-3.0.1.tgz ../../packages/code-server/
+mv code-server-3.0.2.tgz ../../packages/code-server/
 mv index.yaml ../../packages/code-server/
 
 helm repo index ./code-server
